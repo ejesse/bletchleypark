5515389fcb86b989f3c74aff3054867a2bdae561
Jesse Emery <jesse@jesseemery.com>
2015-04-06 23:36:51 -0400

    Refactor and add support for double step

--- enigma/enigmas.py
+++ enigma/enigmas.py
@@ -3,10 +3,38 @@ from enigma import rotors
 
 class Enigma:
 
-    def __init__(self):
+    def __init__(self, rotors=[]):
         self.model_name = None
-        self.rotors = []
+        self.rotors = rotors
         self.reflector = None
+        self._rotors_initialized=False
+        self._init_rotors()
+
+    def _init_rotors(self):
+        if self._rotors_initialized:
+            return
+        if len(self.rotors) > 0:
+            for r in self.rotors:
+                r._enigma = self
+            r = self.rotors[0]
+            r._is_first = True
+            self._rotors_initialized=True
+
+    def check_turnovers(self):
+        for i in range(len(self.rotors)-1,-1,-1):
+            rotor = self.rotors[i]
+            rotor.check_turnover()
+
+    def turnover_signal(self, sending_rotor):
+        position = self.rotors.index(sending_rotor)
+        if position < len(self.rotors):
+            self.rotors[position+1].rotate()
+        # the "double step"
+        # the pawls and ratchets cause this to happen
+        # when the last wheel turns over, making
+        # the previous wheel also turnover
+        if position == (len(self.rotors)-2):
+            self.rotors[position].rotate()
 
     def encrypt_letter(self, letter):
         """
@@ -20,17 +48,21 @@ class Enigma:
         machine ran the circuit from right to left
         from the perspective of the operator
         """
-        cipher = letter
+        cipher = letter.upper()
         for rotor in self.rotors:
             cipher = rotor.right_to_left(cipher)
         cipher = self.reflector.reflect(cipher)
         for i in range(len(self.rotors)-1,-1,-1):
             rotor = self.rotors[i]
             cipher = rotor.left_to_right(cipher)
+        self.check_turnovers()
         return cipher
 
     def encrypt_string(self, plaintext):
+        self.check_turnovers()
         cipher = ''
         for c in plaintext:
             cipher += self.encrypt_letter(c)
         return cipher
+
+
--- enigma/rotors.py
+++ enigma/rotors.py
@@ -18,30 +18,30 @@ REFLECTOR_B_THIN = 'REFLECTOR_B_THIN'
 REFLECTOR_C_THIN = 'REFLECTOR_C_THIN'
 REFLECTOR_ETW = 'REFLECTOR_ETW'
 
+
+def get_next_alpha(letter):
+    letter = letter.upper()
+    if letter == 'Z':
+        return 'A'
+    position = ALPHABET.find(letter)
+    return ALPHABET[position+1]
+
 class Rotor:
 
     def __init__(self, key=''):
+        self.display_state = 'A'
+        self.name = None
         self.rotor_number = None
         self.enigma_model = None
         self.offset = 0
         self.ring_setting = 0
         self._right_circuit = ALPHABET
         self.key = key
-        self.__turnover_position = 0
+        self.turnover_position = 'A'
         self._stepping = 0
         self._left_circuit = None
-
-    @property
-    def turnover_position(self):
-        return self.__turnover_position
-
-    @turnover_position.setter
-    def turnover_position(self, turnover_position):
-        try:
-            turnover_position = turnover_position.upper()
-            self.__turnover_position = ALPHABET.find(turnover_position)
-        except AttributeError:
-            self.__turnover_position = turnover_position
+        self._enigma = None
+        self._is_first = False
 
     def _its_a_circle(self, position):
         if position > 25:
@@ -52,20 +52,24 @@ class Rotor:
         carryover = item[0]
         return item[1:] + carryover
 
-    def _rotate(self):
+    def set_starting_position(self, letter):
+        for i in range(0,ALPHABET.find(letter)):
+            self.rotate()
+
+    def check_turnover(self):
+        if self.display_state == self.turnover_position:
+            self._enigma.turnover_signal(self)
+
+    def rotate(self):
         if self._left_circuit is None:
             self._left_circuit = str(ALPHABET)
         self._left_circuit = self._carryover(self._left_circuit)
         self.key = self._carryover(self.key)
-
-    def step(self):
-        self._stepping+=1
-        if self._stepping > self.__turnover_position:
-            self._stepping = 0
-            self._rotate()
+        self.display_state = get_next_alpha(self.display_state)
 
     def right_to_left(self, letter):
-        self.step()
+        if self._is_first:
+            self.rotate()
         position = self._right_circuit.find(letter)
         if self._left_circuit is not None:
             cipher = self.key[position]
@@ -90,48 +94,60 @@ class Rotor:
     def rotor_for_name(name):
         if name is 'ENIGMA_I_ROTOR_I':
             r = Rotor(key='EKMFLGDQVZNTOWYHXUSPAIBRCJ')
-            r.turnover_position = 17
+            r.name = 'ENIGMA_I_ROTOR_I'
+            r.turnover_position = 'Q'
             return r
         elif name is 'ENIGMA_I_ROTOR_II':
             r = Rotor(key='AJDKSIRUXBLHWTMCQGZNPYFVOE')
-            r.turnover_position = 5
+            r.name = 'ENIGMA_I_ROTOR_II'
+            r.turnover_position = 'E'
             return r
         elif name is 'ENIGMA_I_ROTOR_III':
             r = Rotor(key='BDFHJLCPRTXVZNYEIWGAKMUSQO')
-            r.turnover_position = 0
-            return r
-        elif name is 'ENIGMA_I_ROTOR_III':
-            r = Rotor(key='BDFHJLCPRTXVZNYEIWGAKMUSQO')
+            r.name = 'ENIGMA_I_ROTOR_III'
+            r.turnover_position = 'V'
             return r
         elif name is 'ENIGMA_I_ROTOR_IV':
             r = Rotor(key='ESOVPZJAYQUIRHXLNFTGKDCMWB')
+            r.name = 'ENIGMA_I_ROTOR_IV'
+            r.turnover_position = 'J'
             return r
         elif name is 'ENIGMA_I_ROTOR_V':
             r = Rotor(key='VZBRGITYUPSDNHLXAWMJQOFECK')
+            r.name = 'ENIGMA_I_ROTOR_V'
+            r.turnover_position = 'Z'
             return r
         elif name is 'REFLECTOR_BETA':
             r = Rotor(key='LEYJVCNIXWPBQMDRTAKZGFUHOS')
+            r.name = 'REFLECTOR_BETA'
             return r
         elif name is 'REFLECTOR_GAMMA':
             r = Rotor(key='FSOKANUERHMBTIYCWLQPZXVGJD')
+            r.name = 'REFLECTOR_GAMMA'
             return r
         elif name is 'REFLECTOR_A':
             r = Rotor(key='EJMZALYXVBWFCRQUONTSPIKHGD')
+            r.name = 'REFLECTOR_A'
             return r
         elif name is 'REFLECTOR_B':
             r = Rotor(key='YRUHQSLDPXNGOKMIEBFZCWVJAT')
+            r.name = 'REFLECTOR_B'
             return r
         elif name is 'REFLECTOR_C':
             r = Rotor(key='FVPJIAOYEDRZXWGCTKUQSBNMHL')
+            r.name = 'REFLECTOR_C'
             return r
         elif name is 'REFLECTOR_B_THIN':
             r = Rotor(key='ENKQAUYWJICOPBLMDXZVFTHRGS')
+            r.name = 'REFLECTOR_B_THIN'
             return r
         elif name is 'REFLECTOR_C_THIN':
             r = Rotor(key='RDOBJNTKVEHMLFCWZAXGYIPSUQ')
+            r.name = 'REFLECTOR_C_THIN'
             return r
         elif name is 'REFLECTOR_ETW':
             r = Rotor(key='ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+            r.name = 'REFLECTOR_ETW'
             return r
         else:
             return None
